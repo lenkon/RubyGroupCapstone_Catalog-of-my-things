76333c96707c8c5ee33601dda754bff111b7ca2a
Dilsher <dilsherb@gmail.com>
2023-08-08 13:43:08 +0300

    MusicAlbum class fine tuned.

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,14 +1,19 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify, :can_be_archived
+  attr_accessor :genre, :on_spotify
 
-  def initialize(on_spotify, can_be_archived)
+  def initialize(date, genre, on_spotify)
+    super(date)
+    @genre = genre
     @on_spotify = on_spotify
-    @can_be_archived = can_be_archived
   end
 
   def can_be_archived?
-    super || (@on_spotify == true && @can_be_archived == true)
+    super && @on_spotify
+  end
+
+  def move_to_archive
+    self.archived = can_be_archived?
   end
 end
