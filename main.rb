6bce71920e74191c368e524138c470f7a98b75e7
baqar-abbas <baqar007@hotmail.com>
2023-08-08 13:43:31 +0500

    defined options hash in main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
+
+OPTIONS = {
+  1 => :list_books,
+  2 => :list_music_albums,
+  3 => :list_games,
+  4 => :list_genres,
+  5 => :list_labels,
+  6 => :list_authors,
+  7 => :add_book,
+  8 => :add_music_album,
+  9 => :add_game,
+  10 => :exit
+}.freeze
+
 def main
   loop do
     run
