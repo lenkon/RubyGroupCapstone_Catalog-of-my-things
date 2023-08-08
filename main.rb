9efa4cecb155a89a2e0ddff46ced22230e6a38ea
Dilsher <dilsherb@gmail.com>
2023-08-08 11:46:44 +0300

    show menu to user.

--- main.rb
+++ main.rb
@@ -1,3 +1,16 @@
+def run
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all music albums'
+  puts '3 - List all games'
+  puts '4 - List all genres'
+  puts '5 - List all labels'
+  puts '6 - List all authors'
+  puts '7 - Add a book'
+  puts '8 - Add a music album'
+  puts '9 - Add a game'
+  puts '10 - Exit'
+end
 
 OPTIONS = {
   1 => :list_books,
