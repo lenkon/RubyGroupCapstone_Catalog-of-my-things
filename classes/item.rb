871ae659fc98d7b1e5c16fb04109c25ff4e9d852
baqar-abbas <baqar007@hotmail.com>
2023-08-08 21:03:35 +0500

    Put @publish_date instead of publish_date

--- classes/item.rb
+++ classes/item.rb
@@ -17,7 +17,7 @@ class Item
 
   def can_be_archived?
     # @publish_date < 10.years.ago equivalent statement below
-    publish_date < Time.now - (10 * 365 * 24 * 60 * 60) # 10 years in seconds
+    @publish_date < Time.now - (10 * 365 * 24 * 60 * 60) # 10 years in seconds
   end
 
   def move_to_archive
