489540fe15703c4dd8c43521938547fc0e00540a
Dilsher <dilsherb@gmail.com>
2023-08-08 13:39:43 +0300

    Genre class created with necessary properties and add_item method

---
+++ classes/genre.rb
@@ -0,0 +1,17 @@
+require_relative 'item'
+
+class Genre
+  attr_accessor :name, :items
+  
+  def initialize(name)
+    @name = name
+    @id = rand(1..1000)
+    @items = []
+  end
+
+  def add_item(item)
+    @item << item
+    item.genre = self
+    puts "Added #{item.title} to #{@name}" if $DEBUG == true
+  end
+end
